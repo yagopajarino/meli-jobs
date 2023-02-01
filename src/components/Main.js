d75e43e370d4c76a772cd1b51c3882865d0c68ca
Yago Pajariño <yagopajarino@outlook.com>
2023-02-01 14:48:05 -0300

    Add Main component

---
+++ src/components/Main.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import Filters from "./Filters.js";
+import Positions from "./Positions.js";
+import api from "../api/api.js";
+import Spinner from "./Spinner.js";
+
+export default function Main() {
+  const [positions, setPositions] = useState([]);
+  const [tableBottom, setTableBottom] = useState(0);
+  const [yscroll, setYScroll] = useState(0);
+  const [location, setLocation] = useState("");
+  const [departament, setDepartament] = useState("");
+  const [seniority, setSeniority] = useState("");
+  const [start, setStart] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const buscarPosiciones = (location, departament, seniority, start = 0) => {
+    api.getJobs(location, departament, seniority, start).then((r) => {
+      if (start == 0) {
+        setPositions(r);
+      } else {
+        setPositions([...positions, ...r]);
+      }
+    });
+  };
+
+  useEffect(() => {
+    setStart(0);
+  }, [location, departament, seniority]);
+
+  useEffect(() => {
+    document.addEventListener("scroll", () =>
+      setYScroll(window.innerHeight + window.scrollY)
+    );
+  }, []);
+
+  useEffect(() => {
+    if (start == 0) {
+      setPositions([]);
+    }
+    setLoading(true);
+    buscarPosiciones(location, departament, seniority, start);
+  }, [location, departament, seniority, start]);
+
+  useEffect(() => {
+    const table = document.querySelector("table");
+    if (table) {
+      const lims = document.querySelector("table").getBoundingClientRect();
+      setTableBottom(lims.bottom + window.scrollY);
+    }
+    setLoading(false);
+  }, [positions]);
+
+  useEffect(() => {
+    if (yscroll >= tableBottom && !loading && positions.length == start + 10) {
+      setStart(start + 10);
+    }
+  }, [yscroll]);
+
+  return (
+    <main>
+      <Filters
+        setLocation={setLocation}
+        setDepartament={setDepartament}
+        setSeniority={setSeniority}
+      />
+      <Positions positions={positions} />
+      {loading ? <Spinner /> : ""}
+    </main>
+  );
+}
