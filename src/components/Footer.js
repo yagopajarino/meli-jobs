98ba14f24e4f15e30add5a6c3f40563cf5a12d19
Yago Pajariño <yagopajarino@outlook.com>
2023-02-01 14:48:17 -0300

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import Github from "./Github.js";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>2023</p>
+      <Github />
+      <a href="https://twitter.com/yagopajarino">@yagopajarino</a>
+    </footer>
+  );
+}
---
+++ src/components/Github.js
@@ -0,0 +1,9 @@
+import logo from "../images/github-mark-white.svg";
+
+export default function Github() {
+  return (
+    <a href="https://github.com/yagopajarino/meli-jobs">
+      <img src={logo} width={25}></img>
+    </a>
+  );
+}
