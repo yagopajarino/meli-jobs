cb5f802d09e60fd1d3f5a5b1f38b73a8fa89067d
Yago Pajariño <yagopajarino@outlook.com>
2023-02-01 14:48:38 -0300

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from "./components/Header.js";
+import Main from "./components/Main.js";
+import Footer from "./components/Footer.js";
+
 function App() {
-  return <h1>Hola</h1>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
