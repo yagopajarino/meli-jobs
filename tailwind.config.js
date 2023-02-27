91bb701cd9eea61aea8f377ac553b2e29033b20b
Yago Pajariño <yagopajarino@outlook.com>
2023-02-27 13:28:30 -0300

    Add Tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "ui-sans-serif"],
+    },
+  },
+  plugins: [],
+};
