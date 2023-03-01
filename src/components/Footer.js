b3e0fe79080c53e0bbff0ddf8e8496f497f85d16
Yago Pajariño <yagopajarino@outlook.com>
2023-03-01 19:02:04 -0300

    Change Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,13 @@ import Github from "./Github.js";
 
 export default function Footer() {
   return (
-    <footer>
-      <p>2023</p>
-      <Github />
-      <a href="https://twitter.com/yagopajarino">@yagopajarino</a>
+    <footer className="bg-neutral-800 w-full mt-5 text-lg text-white">
+      <div className="flex flex-row py-5 items-center justify-center space-x-3">
+        <Github />
+        <a href="https://twitter.com/yagopajarino" className="text-gray-400">
+          @yagopajarino
+        </a>
+      </div>
     </footer>
   );
 }
