0c045d58746f376cf57fc90596da83eaefd77963
Yago Pajariño <yagopajarino@outlook.com>
2023-03-01 19:02:23 -0300

    Update Spinner component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="flex flex-col w-3/4 justify-around items-center my-8 p-8 rounded-lg shadow-lg bg-slate-50">
+    <div className="flex flex-col w-full justify-around items-center my-5 p-8 bg-slate-50">
       <div className="spinner">
         <div className="bounce1"></div>
         <div className="bounce2"></div>
