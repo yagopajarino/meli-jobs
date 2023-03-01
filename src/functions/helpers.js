87b22a3b06248e90d2ad1f8edff52ad8d16ada26
Yago Pajariño <yagopajarino@outlook.com>
2023-03-01 19:00:58 -0300

    Add helper isValidUrl

--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -7,9 +7,23 @@ const getJobLink = (pid) => {
   return `https://mercadolibre.eightfold.ai/careers?pid=${pid}`;
 };
 
+const isValidUrl = (urlString) => {
+  var urlPattern = new RegExp(
+    "^(https?:\\/\\/)?" + // validate protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // validate fragment locator
+  return !!urlPattern.test(urlString);
+};
+
 const helpers = {
   getDateTimeStringFromTimestamp,
   getJobLink,
+  isValidUrl,
 };
 
 export default helpers;
