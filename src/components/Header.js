8e6f60f29984f4c1876182c824a71441292ae276
Yago Pajariño <yagopajarino@outlook.com>
2023-03-06 19:13:34 -0300

    Add Header.js content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 export default function Header() {
   return (
-    <header>
-      <h1>Meli Jobs</h1>
+    <header className="text-center  w-full py-8 bg-yellow-300">
+      <h1 className=" text-6xl py-2">Meli Jobs</h1>
+      <p className="text-gray-600 text-xl">
+        Obtené la fecha de publicación de las búsquedas activas de{" "}
+        <a
+          href="https://careers-meli.mercadolibre.com/en"
+          className=" text-blue-700"
+          target="_blank"
+        >
+          mercadolibre
+        </a>
+      </p>
     </header>
   );
 }
