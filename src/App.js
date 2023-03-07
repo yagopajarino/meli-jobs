c56b0929a282278872f874c187d0a700a53c029d
Yago Pajariño <yagopajarino@outlook.com>
2023-03-07 18:58:42 -0300

    Add style to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 import Footer from "./components/Footer.js";
+import Spinner from "./components/Spinner.js";
 
 function App() {
   return (
-    <>
+    <div className=" font-roboto flex flex-col items-center">
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
